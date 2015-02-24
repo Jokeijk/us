9867739a364e2005da8ee427baee5716cf84e1e9
KevinLinghu <daiqilinghu@gmail.com>
2015-02-23 22:02:44 -0800

    use logistic regression for ensemble selection

--- src/dlinghu_Ensemble.py
+++ src/dlinghu_Ensemble.py
@@ -52,7 +52,7 @@ def list_to_array(list):
 # this function sets a validation dataset to run regression on the predictions to get
 # the best linear coefficients to combine models, then combine their predictions on
 # the test dataset.
-def bagging_ensemble(estimators, valid_ratio, x_train, y_train, x_test, n_bootstrap=10):
+def reg_ensemble(estimators, valid_ratio, x_train, y_train, x_test, n_bootstrap=10):
     x_train_sub, x_valid, y_train_sub, y_valid = train_test_split(x_train, y_train, test_size=valid_ratio)
     y_valid_predict_list = manual_bagging(estimators, x_train_sub, y_train_sub, x_valid, bagging=False)
     y_valid_predict_array = list_to_array(y_valid_predict_list)
@@ -73,8 +73,26 @@ def bagging_ensemble(estimators, valid_ratio, x_train, y_train, x_test, n_bootst
     return y_test_predict
 
 
+def ensemble(clf, estimators, valid_ratio, x_train, y_train, x_test):
+    x_train_sub, x_valid, y_train_sub, y_valid = train_test_split(x_train, y_train, test_size=valid_ratio)
+    y_valid_predict_list = manual_bagging(estimators, x_train_sub, y_train_sub, x_valid, bagging=False)
+    y_valid_predict_array = list_to_array(y_valid_predict_list)
+    threshold, y_valid_predict_ensemble = find_reg_threshold(clf, y_valid_predict_array, y_valid)
+    print "Ensembled score = %s" % accuracy_score(y_valid_predict_ensemble, y_valid)
+    print "for comparison, here are single predictor performance:"
+    for i in xrange(y_valid_predict_array.shape[1]):
+        print "score for %s-th predictor = %s" \
+              % (i, accuracy_score(y_valid_predict_array[:, i], y_valid))
+    # now predict test data
+    y_test_predict_list = manual_bagging(estimators, x_train, y_train, x_test, bagging=False)
+    y_test_predict_array = list_to_array(y_test_predict_list)
+    clf.fit(y_valid_predict_array, y_valid)
+    y_test_predict = clf.predict(y_test_predict_array)  # get ensemble result
+    return y_test_predict
+
+
 # def majority_vote(y_predict_list):
-#     y_predict_cumulative = np.zeros((y_predict_list[0].shape[0], 1))
+# y_predict_cumulative = np.zeros((y_predict_list[0].shape[0], 1))
 #     y_predict = np.zeros((y_predict_list[0].shape[0], 1))
 #     for y_predict_sample in y_predict_list:
 #         y_predict_cumulative += y_predict_sample.reshape(-1, 1)
@@ -94,40 +112,42 @@ dt_boosted = AdaBoostClassifier(DecisionTreeClassifier(max_depth=3, max_features
 
 
 
-
-# 0.9430 adaboost_005.py
-
-clf1 = AdaBoostClassifier(
-DecisionTreeClassifier(min_samples_split=500),
- algorithm="SAMME",n_estimators=2000)
-
-# 0.9365 gradient_boost_newnew.py
-
-clf2 = GradientBoostingClassifier(
-            n_estimators=2000,
-            max_depth=3,
-            max_features=None,
-            learning_rate=0.08,
-            random_state=0)
-
-# 0.93750, adaboost_003.py
-clf3 = AdaBoostClassifier(DecisionTreeClassifier(max_depth=3,max_features=100),
-            algorithm="SAMME",
-            n_estimators=700)
-
-# 0.9395, adaboost_001.py
-clf4 = AdaBoostClassifier(DecisionTreeClassifier(max_depth=3),
-            algorithm="SAMME",
-            n_estimators=700)
-
+# ###################################################################
+# # 0.9430 adaboost_005.py
+#
+# clf1 = AdaBoostClassifier(
+#     DecisionTreeClassifier(min_samples_split=500),
+#     algorithm="SAMME", n_estimators=2000)
+#
+# # 0.9365 gradient_boost_newnew.py
+#
+# clf2 = GradientBoostingClassifier(
+#     n_estimators=2000,
+#     max_depth=3,
+#     max_features=None,
+#     learning_rate=0.08,
+#     random_state=0)
+#
+# # 0.93750, adaboost_003.py
+# clf3 = AdaBoostClassifier(DecisionTreeClassifier(max_depth=3, max_features=100),
+#                           algorithm="SAMME",
+#                           n_estimators=700)
+#
+# # 0.9395, adaboost_001.py
+# clf4 = AdaBoostClassifier(DecisionTreeClassifier(max_depth=3),
+#                           algorithm="SAMME",
+#                           n_estimators=700)
+###################################################################
 # estimators = [('SVM', svm), ('Logistic', lr), ('kNN-20', knn),
 # ('DT-boosted', dt_boosted)]
-#estimators = [('SVM', svm), ('Logistic', lr), ('kNN-20', knn)]
-estimators = [('clf1', clf1), ('clf2', clf2), ('clf3', clf3),('clf4',clf4)]
+estimators = [('SVM', svm), ('Logistic', lr), ('kNN-20', knn), ('RF', rf)]
+# estimators = [('clf1', clf1), ('clf2', clf2), ('clf3', clf3), ('clf4', clf4)]
 
 n_bootstrap = 10
 valid_ratio = 0.15  # use 15% training data as validation data
 # n_train = x_train.shape[0]
 # n_test = x_test.shape[0]
-y_test_predict = bagging_ensemble(estimators, valid_ratio, x_train, y_train, x_test)
-output(y_test_predict.reshape(-1), 'dlinghu_Ensemble_001.csv')
+# y_test_predict = reg_ensemble(estimators, valid_ratio, x_train, y_train, x_test)
+y_test_predict = ensemble(linear_model.LogisticRegression(),
+                          estimators, valid_ratio, x_train, y_train, x_test)
+# output(y_test_predict.reshape(-1), 'dlinghu_Ensemble_001.csv')
