d95758e486ae57b4e20f238155686d8631e2c13d
KevinLinghu <daiqilinghu@gmail.com>
2015-03-12 22:49:57 -0700

    Add sample results in folder figure/

--- BSGD/Visualization.py
+++ BSGD/Visualization.py
@@ -46,10 +46,31 @@ S[0:len(s), 0:len(s)] = np.diag(s)
 V_tilde = A[:, :2].transpose().dot(V)
 U_tilde = A[:, :2].transpose().dot(U)
 
+# now find the movies we've watched
+linghu = pd.read_table('linghu.txt', header=None)
+dunzhu = pd.read_table('dunzhu.txt', header=None)
+yiran = pd.read_table('yiran.txt', header=None)
+
 # now visualize the most popular movies
-N = 100
-popular_index = find_popular(movie_all, N)
-plt.scatter(V_tilde[0, popular_index], V_tilde[1, popular_index])
+N = 50
+popular_index = set(find_popular(movie_all, N))
+seen = np.array(list(
+    set(linghu[0]-1).union(set(dunzhu[0]-1)).union(set(yiran[0]-1)).intersection(popular_index)
+))
+movie_seen = movie_all[['title']].iloc[seen].as_matrix()
+plt.scatter(V_tilde[0, seen], V_tilde[1, seen], marker='+', s=50)
+offset = 10
+for label, x, y in zip(movie_seen,
+                       V_tilde[0, seen].transpose(),
+                       V_tilde[1, seen].transpose()):
+    print label
+    plt.annotate(
+        label[0][:-7],
+        xy=(x, y), xytext=(-offset, offset),
+        textcoords='offset points', ha='right', va='bottom',
+        bbox=dict(boxstyle='round,pad=0.5', fc='yellow', alpha=0.5),
+        # arrowprops=dict(arrowstyle='->', connectionstyle='arc3,rad=0')
+    )
 plt.axhline()
 plt.axvline()
 plt.show()
